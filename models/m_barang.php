fde992fb6a023c98c15a15b6b4ba355bfa0e5464
yogi291 <yogileonal@gmail.com>
2021-03-31 22:28:49 +0700

    fitur tambah data

--- models/m_barang.php
+++ models/m_barang.php
@@ -1,6 +1,9 @@
 <?php
 class Barang{
-	private $mysqli;
+
+
+	 private $mysqli;
+	// private $nama,$harga,$stock,$gambar,$db;
 	public function __construct($conn){
 		$this->mysqli = $conn;
 	}
@@ -12,6 +15,15 @@ class Barang{
 		}
 		$query = $db->query($sql) or die ($db->error);
 		return $query;
+	}
+	public function tambah($nm_brg,$hrg_brg,$stc_brg,$gbr_brg){
+		// $this->nama = $nm_brg;
+		// $this->harga = $hrg_brg;
+		// $this->stock = $stc_brg;
+		// $this->gambar = $gbr_brg;
+		$db =$this->mysqli->conn;
+		$db->query("INSERT INTO tb_barang VALUES('','$nm_brg','$hrg_brg','$stc_brg','$gbr_brg')") or die($db->error);
+
 	}
 }
 
--- views/barang.php
+++ views/barang.php
@@ -34,7 +34,7 @@ $barang = new Barang($connection);
   					<td><?php echo $data['nama_brg'] ;?></td>
   					<td><?php echo $data['harga_brg'] ;?></td>
   					<td><?php echo $data['stok_brg'] ;?></td>
-  					<td>akjdhjksdjasd</td>
+  					<td class="text-center"><img src="assets/img/barang/<?php echo $data['gambar_brg'];?>" alt="" width="80px" ></td>
   					<td align="center">
   						<button class="btn btn-info btn-xs"><i class="fa fa-edit"></i> Edit</button>
   						<button class="btn btn-danger btn-xs"><i class="fa fa-trash"></i>  Delete</button>
@@ -45,5 +45,58 @@ $barang = new Barang($connection);
           ?>
   			</table>
   		</div>
+      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#tambah">Tambah Barang</button>
+      <div id="tambah" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close ml-0" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title ">Tambah Data Barang</h4>
+            </div>
+            <form action="" method="post" enctype="multipart/form-data">
+              <div class="modal-body">
+                <div class="form-group">
+                  <label for="nm_brg" class="control-label">Nama Barang</label>
+                  <input type="text" name="nm_brg" class="form-control" id="nm_brg" placeholder="nama-barang" required> 
+                </div>
+                <div class="form-group">
+                  <label for="hrg_brg" class="control-label">Harga Barang</label>
+                  <input type="number" name="hrg_brg" class="form-control" id="hrg_brg" placeholder="Rp." required>
+                </div>
+                <div class="form-group">
+                  <label for="stc_brg" class="control-label">Stock Barang</label>
+                  <input type="number" name="stc_brg" class="form-control" id="stc_brg" placeholder="-" required>
+                </div>
+                <div class="form-group">
+                  <label for="gbr_brg" class="control-label">Gambar barang</label>
+                  <input type="file" class="form-control" name="gbr_brg" id="gbr_brg" accept="image/*" required>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="reset" class="btn btn-danger">reset</button>
+                <input type="submit" class="btn btn-primary" name="tambah" value="simpan">
+              </div>
+            </form>
+            <?php
+            if(@$_POST['tambah']){
+              $nm_brg = $connection->conn->real_escape_string($_POST['nm_brg']);
+              $hrg_brg =  $connection->conn->real_escape_string($_POST['hrg_brg']);
+              $stc_brg = $connection->conn->real_escape_string($_POST['stc_brg']);
+
+              $extensi = explode(".". $_FILES['gbr_brg']['name']);
+              $gbr_brg = "brg-".round(microtime(true)).".".end($extensi);
+              $sumber = $_FILES['gbr_brg']['tmp_name'];
+              $upload = move_uploaded_file($sumber, "assets/img/barang/".$gbr_brg);
+              if($upload){
+                $barang->tambah($nm_brg,$hrg_brg,$stc_brg,$gbr_brg);
+                header("Location: ?page=barang");
+              }else{
+                echo "<script>alert('Upload gambar gagal!')</script>";
+              }
+            }
+            ?>
+          </div>
+        </div>
+      </div>
   	</div>
   </div>
